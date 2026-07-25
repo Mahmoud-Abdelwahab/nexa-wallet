a4339723032ff147be1f98d422816f343adc3c94
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2026-07-25 20:15:03 +0400

    create user model and users table

---
+++ app/Models/user.py
@@ -0,0 +1,26 @@
+from datetime import date, datetime
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    mobile: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    date_of_birth: Mapped[date] = mapped_column(nullable=False)
+    
+    created_at: Mapped[datetime] = mapped_column(
+            default=datetime.now,
+            nullable=False,
+     )
+    
+    updated_at: Mapped[datetime] = mapped_column(
+            default=datetime.now,
+            onupdate=datetime.now,
+            nullable=False,
+        )
