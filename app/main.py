17d4241e60e5489f75e07df11e0078d9e5f975f0
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2026-08-02 16:23:19 +0400

    removing datebase_init.py as we depend on alembic in creating the db and manage it versioning

--- app/main.py
+++ app/main.py
@@ -5,24 +5,10 @@ from app.api.health import router as health_router
 from app.core.config import settings
 
 
-## No need for lifespan now as we alreadt create the DB using Alembic and we will use Alembic to manage the DB Schema and migrations and versioning
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     # Startup
-#     init_db()
-
-#     yield
-
-#      # Shutdown
-#      # cleanup resources later
-
-
-
 # Create FastAPI app and include routers
 app = FastAPI(
     title=settings.app_name,
-    version="1.0.0",
-    lifespan=lifespan,
+    version="1.0.0"
 )
 
 app.include_router(health_router)
