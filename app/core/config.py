6d83717b6a1a4d281829ac00ccb970521c398bfc
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2026-08-03 19:56:43 +0400

    prepare the main structure

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# This file used to read environment variables from .env file 
+# This file used to read environment variables from .env file
 # for production we don't store keys in .env , we use aws secrets manager or other secure vaults to store keys and secrets.
+
 
 class Settings(BaseSettings):
     app_name: str = "Nexa Wallet"
     debug: bool = True
     database_url: str = "postgresql+psycopg://nexa_user:nexa_password@localhost:5432/nexa_wallet"
-    # SECRET_KEY: str = "your-secret-key-change-in-production"
-    # ALGORITHM: str = "HS256"
-    # ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
     model_config = SettingsConfigDict(
         env_file=".env",
