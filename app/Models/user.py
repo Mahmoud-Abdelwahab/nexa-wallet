5043ba016dd23b31befb6fa60fad38910bd4d882
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2026-08-16 17:09:43 +0400

    adding new token version to user table to validate token after password change

--- app/Models/user.py
+++ app/Models/user.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -55,3 +55,13 @@ class User(Base):
     wallets: Mapped[list["Wallet"]] = relationship(
         back_populates="user",
     )
+    #  token_version is used to invalidate JWT tokens. When a user changes their password,
+    #  we increment the token_version in the database.
+    #  The JWT token will include the token_version at the time of issuance.
+    #  When validating the token, we compare the token_version in the token with the current token_version in the database.
+    # If they don't match, it means the user's password has changed since the token was issued, and we reject the token.
+    token_version: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=0,
+   )
