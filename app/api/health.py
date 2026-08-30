459408a51076221391288a8ae0b9048a3b46d17d
Mahmoud abdelwahab <mahmoud.elziany@gmail.com>
2026-08-30 14:48:24 +0400

    testing redis is working fine

--- app/api/health.py
+++ app/api/health.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+from app.core.redis import redis_client
 router = APIRouter(prefix="/health", tags=["Health"])
 # /health endpoint is the prefix for all routes in this router. The tags parameter  Health is used to group the endpoints in the API documentation.
 
@@ -8,4 +8,10 @@ async def health():
     return {
         "status": "ok",
         "service": "Nexa Wallet API"
+    }
+
+@router.get("/health/redis")
+async def redis_health():
+    return {
+        "redis": await redis_client.ping()
     }
